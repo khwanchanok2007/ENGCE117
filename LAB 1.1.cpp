67b3d8bcfd2efcba48ec226ee05bf0821fa671c3
@˚.˙ ᴋʜᴡᴀɴᴄʜᴀɴᴏᴋ 𖦹  ˙𓃠˚ <khwanchanoktafai@gmail.com>
2025-12-05 18:12:20 +0700

    Update LAB 1.1.cpp

--- LAB 1.1.cpp
+++ LAB 1.1.cpp
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main() {
+    int N, i;
+
+    if(scanf("%d",&N) != 1) {
+        return 1;
+    }
+
+    for (i = 1; i <= N; i++)
+    {
+        printf("[%d]Hello Word!\n",i);
+    }
+    
+}
